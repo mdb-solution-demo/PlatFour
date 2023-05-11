66328c9e5d1e7e6625a3a56356c480c360d1b97f
binchow <binchow@gmail.com>
2023-05-11 19:28:17 +0800

    a

--- hosting/files/main.js
+++ hosting/files/main.js
@@ -16,7 +16,7 @@ Vue.directive('visible', function (el, binding) {
 
 async function initRealmApp() {
     "use strict";
-    realmApp = new Realm.App({ id: "platfour-qhfdw" });
+    realmApp = new Realm.App({ id: "platfour-ccucq" });
     //Dont log in again  if we are logged in
     realmUser = realmApp.currentUser
     if (!realmUser) {
